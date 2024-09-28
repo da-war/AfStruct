d34df1f24b651d47d1dc40f523dd1076f337d2cb
Rana Dawar Abdullah <imdawar090@gmail.com>
2024-09-28 11:10:08 +0500

    Array

--- main.go
+++ main.go
@@ -3,10 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	result := add("1", " 2")
-	fmt.Println(result)
-}
+	prices := [3]float64{1.1, 2.2, 3.3}
 
-func add[T int | float64 | string](a, b T) T {
-	return a + b
+	fmt.Println(prices[0])
 }
