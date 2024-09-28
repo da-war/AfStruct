29aa21075faf6fa9b2cb0788e1acedcffd4e783f
Rana Dawar Abdullah <imdawar090@gmail.com>
2024-09-28 12:21:22 +0500

    Maps

--- main.go
+++ main.go
@@ -4,4 +4,25 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
+	websites := map[string]string{
+		"SitePoint": "https://www.sitepoint.com",
+		"Google":    "https://www.google.com",
+		"Facebook":  "https://www.facebook.com",
+	}
+
+	fmt.Println(websites["SitePoint"])
+	fmt.Println(websites["Google"])
+
+	fmt.Println(websites)
+	fmt.Println("sdfsafdsa", websites["Instagram"])
+
+	websites["Instagram"] = "https://www.instagram.com"
+
+	fmt.Println(websites)
+	fmt.Println(websites["Instagram"])
+
+	delete(websites, "SitePoint")
+
+	fmt.Println(websites)
+
 }
