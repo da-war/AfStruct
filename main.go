700135d768adea0f21be3617c45391af68c32f6f
Rana Dawar Abdullah <imdawar090@gmail.com>
2024-09-28 11:03:53 +0500

    Generics done

--- main.go
+++ main.go
@@ -3,33 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	result := add(1, 2)
+	result := add("1", " 2")
 	fmt.Println(result)
 }
 
-func add[T any](a, b T) T {
-	//type check
-	aInt, aIsInt := a.(int)
-	bInt, bIsInt := b.(int)
-
-	if aIsInt && bIsInt {
-		return aInt + bInt
-	}
-
-	aFloat, aIsFloat := a.(float64)
-	bFloat, bIsFloat := b.(float64)
-
-	if aIsFloat && bIsFloat {
-		return aFloat + bFloat
-	}
-
-	aString, aIsString := a.(string)
-	bString, bIsString := b.(string)
-
-	if aIsString && bIsString {
-		return aString + bString
-	}
-
-	return a
-
+func add[T int | float64 | string](a, b T) T {
+	return a + b
 }
